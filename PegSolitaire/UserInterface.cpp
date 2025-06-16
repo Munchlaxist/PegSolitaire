d73d7293ee869259d19c6a199914fef886c096d4
Mika <mika-99@gmx.de>
2025-06-16 11:55:35 +0200

    Implemented getRenderWindow() method and moved gameLoop() to main.cpp

--- PegSolitaire/UserInterface.cpp
+++ PegSolitaire/UserInterface.cpp
@@ -27,6 +27,11 @@ UserInterface::UserInterface(GameLogic& gameLogic) : m_gameLogic{ gameLogic } {
 }
 
 
+sf::RenderWindow& UserInterface::getRenderWindow() {
+	return m_window;
+}
+
+
 void UserInterface::setBackground(std::filesystem::path filename) {
 	m_window.clear();
 	sf::Texture backgroundTexture; // Create texture for the background
@@ -98,116 +103,5 @@ void UserInterface::createTryAgainButton() {
 		m_window.draw(tryAgainButton);
 		m_window.draw(buttonText);
 		m_window.display();
-	}
-}
-
-
-void UserInterface::gameLoop() {
-	// TODO: Implement music input as seperate function
-	sf::Music gameWon("game_won.mp3");
-	sf::Music gameLost("game_lost.mp3");
-	sf::Music correctMove("correct_move.mp3");
-
-	// TODO: Implement winning/losing message as seperate function
-	sf::Font font;
-	if (!font.openFromFile("arial.ttf")) {
-		throw std::runtime_error("Could not load font: arial.ttf");
-	}
-	sf::Text gameWonText(font, "Congratulations!", 50);
-	gameWonText.setFillColor(sf::Color::Blue); // Set text color
-	gameWonText.setPosition(sf::Vector2f(200.f, 20.f)); // Set text position
-	sf::Text gameLostText(font, "You lost, try again!", 50);
-	gameLostText.setFillColor(sf::Color::Red); // Set text color
-	gameLostText.setPosition(sf::Vector2f(200.f, 20.f)); // Set text position
-	
-
-	drawBoard(); // Draw the initial board
-	GameState gameState = GameState::Playing;
-
-	while (m_window.isOpen())
-	{
-		while (const std::optional event = m_window.pollEvent())
-		{
-
-			if (event->is<sf::Event::Closed>()) {
-				m_window.close();
-			}
-
-			if (gameState == GameState::Playing) {
-				
-				if (const auto* buttonPressed = event->getIf<sf::Event::MouseButtonPressed>()) {
-					if (buttonPressed->button == sf::Mouse::Button::Left) {
-						// Handle left mouse button pressed events here
-						sf::Vector2i mousePosition = sf::Mouse::getPosition(m_window);
-						if (auto* peg = getClickedField(mousePosition)) {
-							std::cout << "Mouse button pressed at: " << mousePosition.x << ", " << mousePosition.y << std::endl; // Debugging output
-
-							// Check if there already is a field selected -> if so, check if the move is valid, if not mark current field as selected (if it is occupied)
-							if (auto* selectedPeg = getCurrentSelectedField()) {
-								if (peg->getState() == FieldState::Empty) {
-									if (m_gameLogic.isValidMove(*selectedPeg, *peg)) {
-										m_gameLogic.makeMove(*selectedPeg, *peg);
-										syncBoard(); // Update the board based on the move, i.e. the map of fields to their corresponding circles
-										drawBoard();
-										if (m_gameLogic.solutionFound()) {
-											gameWon.play(); // Play game won sound
-											std::cout << "Solution found! Congratulations!" << std::endl;
-											gameState = GameState::GameOver; // TODO actually you won, that needs to be adapted later on
-											syncBoard();
-											drawBoard();
-											m_window.draw(gameWonText); // Draw the game won text
-											createTryAgainButton(); // Draw the try again button
-											break;
-										}
-										if (!m_gameLogic.movesAvailable()) {
-											gameLost.play(); // Play game lost sound
-											std::cout << "No moves available! Game over!" << std::endl;
-											gameState = GameState::GameOver;
-											syncBoard();
-											drawBoard();
-											m_window.draw(gameLostText); // Draw the game lost text
-											createTryAgainButton(); // Draw the try again button
-											break;
-										}
-										correctMove.play(); // Play correct move sound (if the game was not won or lost)
-									}
-									else {
-										selectedPeg->setState(FieldState::Occupied);
-										syncBoard();
-										drawBoard();
-									}
-								}
-								else {
-									selectedPeg->setState(FieldState::Occupied);
-									syncBoard();
-									drawBoard();
-								}
-							}
-							else {
-								if (peg->getState() == FieldState::Occupied) {
-									peg->setState(FieldState::Selected);
-									syncBoard();
-									drawBoard();
-								}
-							}
-
-						}
-					}
-				}
-			}
-			else { // If tha game is over, handle the try again button click event
-				if (const auto* buttonPressed = event->getIf<sf::Event::MouseButtonPressed>()) {
-					if (buttonPressed->button == sf::Mouse::Button::Left) {
-						sf::Vector2i mousePosition = sf::Mouse::getPosition(m_window);
-						if (mousePosition.x >= 10 && mousePosition.x <= 160 && mousePosition.y >= 10 && mousePosition.y <= 60) {
-							m_gameLogic.resetGame(); // Reset the game logic
-							syncBoard(); // Reset the board
-							drawBoard();
-							gameState = GameState::Playing; // Change game state back to playing
-						}
-					}
-				}
-			}
-		}
 	}
 }
